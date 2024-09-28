80ac55cc567f450ee944495c87991b98da1aa6ef
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-09-29 04:28:47 +0800

    Update adduser.js

--- plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/adduser.js
+++ plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/adduser.js
@@ -8,16 +8,16 @@ const config = {
     credits: "XaviaTeam",
 };
 
-function adduser(userID, threadID) {
+const adduser = (userID, threadID) => {
     return new Promise((resolve, reject) => {
         global.api.addUserToGroup(userID, threadID, (err) => {
             if (err) return reject(err);
             resolve();
         });
     });
-}
+};
 
-async function onCall({ message, args, data }) {
+const onCall = async ({ message, args, data }) => {
     if (!message.isGroup) return;
 
     const { threadID, senderID, reply } = message;
@@ -26,24 +26,24 @@ async function onCall({ message, args, data }) {
     if (!input) return reply("You have not entered the ID or profile link of the person to add to the group.");
 
     const { adminIDs } = data.thread.info;
-
-    if (!adminIDs.some(e => e == global.botID))
+    if (!adminIDs.includes(global.botID)) {
         return reply("The bot needs group administration rights to perform this command.");
+    }
 
     let uid = !isNaN(input)
         ? input
-        : input.match(/(?:https?:\/\/)?(?:www.|m.)?(?:facebook|fb).com\/(?:\w)*#!\/?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\.-]+)/)?.[1];
+        : extractUserIDFromURL(input);
 
     if (!uid) return reply("ID or profile link is invalid.");
-    
+
     if (isNaN(uid)) {
         uid = (await api.getUserID(uid))[0]?.userID;
     }
 
     if (!uid || isNaN(uid)) return reply("ID or profile link is invalid.");
 
-    if (uid == global.botID) return reply("The bot cannot add itself to the group.");
-    if (uid == senderID) return reply("You cannot use this command to add yourself to the group.");
+    if (uid === global.botID) return reply("The bot cannot add itself to the group.");
+    if (uid === senderID) return reply("You cannot use this command to add yourself to the group.");
 
     try {
         await adduser(uid, threadID);
@@ -52,7 +52,12 @@ async function onCall({ message, args, data }) {
         console.error(e);
         return reply("An error has occurred, please try again later.");
     }
-}
+};
+
+const extractUserIDFromURL = (url) => {
+    const match = url.match(/(?:https?:\/\/)?(?:www.|m.)?(?:facebook|fb).com\/(?:\w)*#!\/?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\.-]+)/);
+    return match ? match[1] : null;
+};
 
 export default {
     config,
