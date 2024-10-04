c369560213641ec28281b1fe13d6b8ceae8f94b6
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-10-04 19:43:11 +0800

    Update help.js

--- plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/help.js
+++ plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/help.js
@@ -4,7 +4,7 @@ const config = {
     version: "1.0.3",
     description: "Show all commands or command details",
     usage: "[command] (optional)",
-    credits: "XaviaTeam"
+    credits: "coffee"
 };
 
 function getCommandName(commandName) {
@@ -32,7 +32,13 @@ async function onCall({ message, args, userPermissions, prefix }) {
         }
 
         // Arrange categories in the specified order
-        const orderedCategories = ["📖 | 𝙴𝚍𝚞𝚌𝚊𝚝𝚒𝚘𝚗", "🖼 | 𝙸𝚖𝚊𝚐𝚎", "🎧 | 𝙼𝚞𝚜𝚒𝚌", "👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜"];
+        let orderedCategories = ["📖 | 𝙴𝚍𝚞𝚌𝚊𝚝𝚒𝚘𝚗", "🖼 | 𝙸𝚖𝚊𝚐𝚎", "🎧 | 𝙼𝚞𝚜𝚒𝚌", "👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜"];
+
+        // Check if user has permissions 1 or 2 to show the "Owner" category
+        if (userPermissions.includes(1) || userPermissions.includes(2)) {
+            orderedCategories.push("🐢💨 | 𝙾𝚠𝚗𝚎𝚛"); // Add the Owner category at the end
+        }
+
         const commandList = orderedCategories
             .filter(category => commands[category])
             .map(category => `
