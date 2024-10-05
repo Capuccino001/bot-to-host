9ac7a448631ef82bba46c3c7310cc18c52d86a93
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-10-05 15:56:25 +0800

    Update gmage.js

--- plugins/commands/🖼 | 𝙸𝚖𝚊𝚐𝚎/gmage.js
+++ plugins/commands/🖼 | 𝙸𝚖𝚊𝚐𝚎/gmage.js
@@ -1,6 +1,11 @@
 import path from 'path';
 import fs from 'fs-extra';
 import axios from 'axios';
+import { fileURLToPath } from 'url';
+
+// Manually define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config = {
     name: "gmage",
