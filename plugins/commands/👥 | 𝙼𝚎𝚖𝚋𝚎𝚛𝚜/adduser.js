9391f19f44a64bbeac75623d4644e147f482f141
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-10-06 19:43:23 +0800

    Update adduser.js

--- plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/adduser.js
+++ plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/adduser.js
@@ -2,7 +2,7 @@ const config = {
     name: "adduser",
     aliases: ["add"],
     description: "Add user to group",
-    usage: "[uid/profileUrl]",
+    usage: "[uid]",
     cooldown: 3,
     permissions: [0],
     credits: "XaviaTeam",
@@ -21,42 +21,29 @@ const onCall = async ({ message, args, data }) => {
     if (!message.isGroup) return;
 
     const { threadID, senderID, reply } = message;
-    const input = args[0]?.toLowerCase();
+    const input = args[0];
 
-    if (!input) return reply("You have not entered the ID or profile link of the person to add to the group.");
+    if (!input || isNaN(input)) {
+        return reply("You have not entered a valid user ID (uid) to add to the group.");
+    }
 
     const { adminIDs } = data.thread.info;
     if (!adminIDs.includes(global.botID)) {
         return reply("The bot needs group administration rights to perform this command.");
     }
 
-    let uid = !isNaN(input)
-        ? input
-        : extractUserIDFromURL(input);
-
-    if (!uid) return reply("ID or profile link is invalid.");
-
-    if (isNaN(uid)) {
-        uid = (await api.getUserID(uid))[0]?.userID;
-    }
-
-    if (!uid || isNaN(uid)) return reply("ID or profile link is invalid.");
+    const uid = input;
 
     if (uid === global.botID) return reply("The bot cannot add itself to the group.");
     if (uid === senderID) return reply("You cannot use this command to add yourself to the group.");
 
     try {
         await adduser(uid, threadID);
-        return reply("Added successfully.");
+        return reply("User added successfully.");
     } catch (e) {
         console.error(e);
         return reply("An error has occurred, please try again later.");
     }
-};
-
-const extractUserIDFromURL = (url) => {
-    const match = url.match(/(?:https?:\/\/)?(?:www.|m.)?(?:facebook|fb).com\/(?:\w)*#!\/?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\.-]+)/);
-    return match ? match[1] : null;
 };
 
 export default {
