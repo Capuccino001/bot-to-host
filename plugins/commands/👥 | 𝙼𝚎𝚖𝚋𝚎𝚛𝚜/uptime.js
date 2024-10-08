24ffef3e7a18db37d03e48dc5b836c14bf885ba9
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-10-09 00:24:29 +0800

    Update uptime.js

--- plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/uptime.js
+++ plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/uptime.js
@@ -11,18 +11,16 @@ function msToHMS(ms) {
     let hours = Math.floor(seconds / 3600);
     seconds = seconds % 3600;
     let minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
 
     return {
         hours: hours,
-        minutes: minutes,
-        seconds: seconds
+        minutes: minutes
     };
 }
 
 function onCall({ message }) {
     const botUptime = msToHMS(process.uptime() * 1000);
-    const serverUptime = msToHMS(os.uptime() * 1000); // Get server uptime
+    const serverUptime = msToHMS(os.uptime() * 1000); 
 
     message.reply(`────────────────\n»  (⁠ ⁠˘⁠ ⁠³⁠˘⁠)┌旦「 𝙾𝚗𝚕𝚒𝚗𝚎 」«\n[ 𝚂𝚎𝚛𝚟𝚎𝚛 ] ${serverUptime.hours} Hours ${serverUptime.minutes} minutes\n[ 𝙱𝚘𝚝 ] ${botUptime.hours} Hours ${botUptime.minutes} minutes\n────── >ᴗ< ───────`);
 }
