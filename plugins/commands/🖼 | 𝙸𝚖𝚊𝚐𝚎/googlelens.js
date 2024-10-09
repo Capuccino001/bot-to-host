21acb8546e1ae4713861c5a9c47cf7a2204167d5
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-10-09 16:13:04 +0800

    Update googlelens.js

--- plugins/commands/🖼 | 𝙸𝚖𝚊𝚐𝚎/googlelens.js
+++ plugins/commands/🖼 | 𝙸𝚖𝚊𝚐𝚎/googlelens.js
@@ -83,8 +83,7 @@ const onCall = async ({ message }) => {
       imgData.push(stream);
     }));
 
-    const replyMessages = "(⁠｢⁠`⁠･⁠ω⁠･⁠)⁠｢ | 𝐆𝐨𝐨𝐠𝐥𝐞 𝐋𝐞𝐧𝐬 
-・──────────────・\n\n" + results.map((item, index) => 
+    const replyMessages = "(⁠｢`･⁠ω⁠･⁠)⁠｢ | 𝐆𝐨𝐨𝐠𝐥𝐞 𝐋𝐞𝐧𝐬 \n・──────────────・\n\n" + results.map((item, index) => 
       `${index + 1}. ${toBoldFont("Title:")} ${item.title}\n${toBoldFont("Source:")} ${item.source}\n${toBoldFont("Link:")} ${item.link}`
     ).join("\n\n");
 
