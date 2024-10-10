448c195c8d4a9f9e87231cabc22ccda07a673b22
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-10-10 12:07:36 +0800

    Update uptime.js

--- plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/uptime.js
+++ plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/uptime.js
@@ -8,11 +8,14 @@ const config = {
 
 function msToHMS(ms) {
     let seconds = Math.floor(ms / 1000);
+    let days = Math.floor(seconds / 86400);
+    seconds = seconds % 86400;
     let hours = Math.floor(seconds / 3600);
     seconds = seconds % 3600;
     let minutes = Math.floor(seconds / 60);
 
     return {
+        days: days,
         hours: hours,
         minutes: minutes
     };
@@ -22,7 +25,7 @@ function onCall({ message }) {
     const botUptime = msToHMS(process.uptime() * 1000);
     const serverUptime = msToHMS(os.uptime() * 1000); 
 
-    message.reply(`────────────────\n»  (⁠ ⁠˘⁠ ⁠³⁠˘⁠)┌旦「 𝙾𝚗𝚕𝚒𝚗𝚎 」«\n[ 𝚂𝚎𝚛𝚟𝚎𝚛 ] ${serverUptime.hours} Hours ${serverUptime.minutes} minutes\n[ 𝙱𝚘𝚝 ] ${botUptime.hours} Hours ${botUptime.minutes} minutes\n────── >ᴗ< ───────`);
+    message.reply(`────────────────\n»  (⁠ ⁠˘⁠ ⁠³⁠˘⁠)┌旦「 𝙾𝚗𝚕𝚒𝚗𝚎 」«\n[ 𝚂𝚎𝚛𝚟𝚎𝚛 ] ${serverUptime.days}D ${serverUptime.hours}H ${serverUptime.minutes}M\n[ 𝙱𝚘𝚝 ] ${botUptime.hours} Hours ${botUptime.minutes} minutes\n────── >ᴗ< ───────`);
 }
 
 export default {
