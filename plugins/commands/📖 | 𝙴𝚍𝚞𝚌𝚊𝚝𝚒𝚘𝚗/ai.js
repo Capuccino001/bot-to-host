a9c394ac116a657359ba431c30fef045709dac9d
Capuccino <168435552+Capuccino001@users.noreply.github.com>
2024-10-13 07:32:59 +0800

    Update ai.js

--- plugins/commands/📖 | 𝙴𝚍𝚞𝚌𝚊𝚝𝚒𝚘𝚗/ai.js
+++ plugins/commands/📖 | 𝙴𝚍𝚞𝚌𝚊𝚝𝚒𝚘𝚗/ai.js
@@ -22,13 +22,13 @@ async function onCall({ message, args }) {
         const attachment = message.messageReply.attachments[0];
         const imageURL = attachment.url;
 
-        const geminiUrl = `https://joncll.serv00.net/chat.php?ask=${encodeURIComponent(query)}&imgurl=${encodeURIComponent(imageURL)}`;
+        const geminiUrl = `https://deku-rest-apis.ooguy.com/gemini?prompt=describe%20this%20photo&url=${encodeURIComponent(imageURL)}`;
         try {
             const response = await axios.get(geminiUrl);
-            const { vision } = response.data;
+            const { gemini } = response.data;
 
-            if (vision) {
-                return await message.reply(`${header}\n${vision}\n${footer}`);
+            if (gemini) {
+                return await message.reply(`${header}\n${gemini}\n${footer}`);
             } else {
                 return await message.reply(`${header}\nFailed to recognize the image.\n${footer}`);
             }
